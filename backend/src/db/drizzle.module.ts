6fc67b1820b920b0d8d6d875925b8767c731a0d6
chyun7114 <chyun7114@naver.com>
2025-11-10 22:28:50 +0900

    Feat(Backend): PostgreSQL 및 Redis 설정 완료

---
+++ backend/src/db/drizzle.module.ts
@@ -0,0 +1,10 @@
+import { Global, Module } from '@nestjs/common';
+import { drizzleProvider } from './drizzle.provider';
+import { DrizzleService } from './drizzle.service';
+
+@Global()
+@Module({
+  providers: [drizzleProvider, DrizzleService],
+  exports: [drizzleProvider, DrizzleService],
+})
+export class DrizzleModule {}
