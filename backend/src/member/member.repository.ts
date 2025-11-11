1b3d0752494e374cda1fffb5b1f7184fd4ddb81e
chyun7114 <chyun7114@naver.com>
2025-11-11 16:34:43 +0900

    Feat(Backend): 회원가입/로그인을 위한 repository 코드 구현

---
+++ backend/src/member/member.repository.ts
@@ -0,0 +1,51 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { DRIZZLE_ORM } from '../common/db';
+import { drizzle } from 'drizzle-orm/postgres-js';
+import * as schema from '../common/db/schema';
+import { Member } from './entity';
+import { eq } from 'drizzle-orm';
+
+type DrizzleDB = ReturnType<typeof drizzle<typeof schema>>;
+
+@Injectable()
+export class MemberRepository {
+    constructor(
+        @Inject(DRIZZLE_ORM)
+        private readonly db: DrizzleDB,
+    ) {}
+
+    async createMember(data: {
+        username: string;
+        password: string;
+        name: string;
+    }) {
+        const [member] = await this.db
+            .insert(Member)
+            .values({
+                username: data.username,
+                password: data.password,
+                name: data.name,
+            })
+            .returning();
+
+        return member;
+    }
+
+    async findById(id: number) {
+        const [member] = await this.db
+            .select()
+            .from(Member)
+            .where(eq(Member.id, id));
+
+        return member;
+    }
+
+    async findByEmail(email: string) {
+        const [member] = await this.db
+            .select()
+            .from(Member)
+            .where(eq(Member.email, email));
+
+        return member;
+    }
+}
