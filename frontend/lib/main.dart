baeabdc12e479c0abc0f3b8e6a0b1bbf00514d40
chyun7114 <chyun7114@naver.com>
2025-11-11 17:49:28 +0900

    Feat(Shared): 로그인/회원가입 API 연결

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'providers/game_provider.dart';
+import 'providers/auth_provider.dart';
 import 'screens/login_screen.dart';
 
-void main() {
+Future<void> main() async {
+  // .env 파일이 있을 때만 로드 (개발 환경)
+  try {
+    await dotenv.load(fileName: ".env");
+  } catch (e) {
+    // .env 파일이 없어도 계속 진행 (프로덕션 환경)
+    print('기본 설정을 사용합니다.');
+  }
   runApp(const TetrisApp());
 }
 
@@ -12,8 +21,11 @@ class TetrisApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => GameProvider(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => GameProvider()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+      ],
       child: MaterialApp(
         title: 'Tetris',
         debugShowCheckedModeBanner: false,
