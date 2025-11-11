f44d9440257ecbbcc8d636b249ac49530337a007
chyun7114 <chyun7114@naver.com>
2025-11-11 16:34:43 +0900

    Feat(Backend): 회원가입/로그인을 위한 repository 코드 구현

--- backend/drizzle/migrations/0000_moaning_havok.sql
+++ backend/drizzle/migrations/0000_moaning_havok.sql
@@ -6,6 +6,9 @@ CREATE TABLE "member" (
 	"username" text NOT NULL,
 	"email" text NOT NULL,
 	"password" text NOT NULL,
+<<<<<<<< HEAD:backend/drizzle/migrations/0000_equal_thor.sql
 	CONSTRAINT "member_username_unique" UNIQUE("username"),
+========
+>>>>>>>> a56e290 (Feat(Backend): 회원가입/로그인을 위한 repository 코드 구현):backend/drizzle/migrations/0000_moaning_havok.sql
 	CONSTRAINT "member_email_unique" UNIQUE("email")
 );
--- backend/src/member/entity/member.entity.ts
+++ backend/src/member/entity/member.entity.ts
@@ -1,9 +1,9 @@
-import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, text } from 'drizzle-orm/pg-core';
 import { baseColumns } from 'src/common/db/base.entity';
 
 export const Member = pgTable('member', {
     ...baseColumns,
-    username: text('username').notNull().unique(),
+    username: text('username').notNull(),
     email: text('email').notNull().unique(),
     password: text('password').notNull(),
 });
